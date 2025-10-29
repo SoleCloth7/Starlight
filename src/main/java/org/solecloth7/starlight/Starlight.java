053253639ce95d2441d393bc698bc2dadea0af2d
SoleCloth7 <solecloth7@gmail.com>
2025-10-29 17:42:53 +0100

    Initial

---
+++ src/main/java/org/solecloth7/starlight/Starlight.java
@@ -0,0 +1,17 @@
+package org.solecloth7.starlight;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Starlight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
